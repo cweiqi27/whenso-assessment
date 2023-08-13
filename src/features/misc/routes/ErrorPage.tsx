5746fd582290c89e0527b31e609f52ca48c1dfc7
Chan Wei Qi <chanweiqi27@gmail.com>
2023-08-14 06:07:28 +0800

    style: add bg color

--- src/features/misc/routes/ErrorPage.tsx
+++ src/features/misc/routes/ErrorPage.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export const ErrorPage = () => {
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center gap-4">
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-4 bg-slate-200">
       <h1 className="text-6xl font-black sm:text-9xl">Oops!</h1>
       <p className="text-2xl font-bold">Something went wrong.</p>
       <Link to="/">Back to start page</Link>
